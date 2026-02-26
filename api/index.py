554f5c9dbc5da0bc7fc32b95bb372919d5b80d22
algeneral24 <150769392+algeneral24@users.noreply.github.com>
2026-02-26 21:01:09 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,14 @@
-import sys
-import os
-
-# إضافة المسار الرئيسي للـ PATH
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from newe import app as application
-
-# Vercel يتوقع متغير باسم 'app'
-app = application
+{
+  "builds": [
+    {
+      "src": "api/index.py",
+      "use": "@vercel/python"
+    }
+  ],
+  "routes": [
+    {
+      "src": "/(.*)",
+      "dest": "/api/index.py"
+    }
+  ]
+}
